e97f7db751b41e07dceb8ab51c6eb5693c79f400
BANKA2017 <17263253+BANKA2017@users.noreply.github.com>
2022-07-20 13:12:51 +0800

    update: account list in staff candle stick page

--- src/views/topics/staffCandleStickPage.vue
+++ src/views/topics/staffCandleStickPage.vue
@@ -56,6 +56,7 @@ export default defineComponent({
         "bang_dream_info",
         "bang_dream_gbp",
         "bangdreamgbp_EN",
+        "bangdreamparty_",
       ],
       "MyGO!!!!!": ["bang_dream_mygo"],
       "ARGONAVIS": ["ARGONAVIS_info"],
