83e30dd27ca214b0ddd6867cbb9bd31301c792f7
BANKA2017 <17263253+BANKA2017@users.noreply.github.com>
2023-04-02 23:21:28 +0800

    update: polls component because online api will provide real time data for polls

--- src/components/TwPolls.vue
+++ src/components/TwPolls.vue
@@ -7,12 +7,12 @@
             <el-image alt="pollImage" :preview-src-list="[createRealMediaPath(realMediaPath, samePath, 'tweets')+media[0].url+'']" :src="createRealMediaPath(realMediaPath, samePath, 'tweets')+media[0].url+''" class="col-12 card-img-top" fit="cover" lazy style="height: 300px"></el-image>
           </div>
         </div>
-        <div v-if="etaSeconds <= 0 && ((!settings.onlineMode && polls[0].checked) || (settings.onlineMode && state.updateFlag && state.polls.length)) && pollCount > 0" class="col-12">
+        <div v-if="(etaSeconds <= 0 && ((!settings.onlineMode && polls[0].checked)) || ((settings.onlineMode && polls.length)) && pollCount > 0)" class="col-12">
           <div class="progress" v-for="(poll, index) in polls" :key="poll.poll_order">
-            <div :style="{'background-color': maxPollIndex === index ? '#7CC5F6' : '#CFD9DE',  width: Math.ceil(((settings.onlineMode ? state.polls[index] : poll.count)/pollCount)*100) + '%'}" class="progress-bar"></div>
+            <div :style="{'background-color': maxPollIndex === index ? '#7CC5F6' : '#CFD9DE',  width: Math.ceil((poll.count/pollCount)*100) + '%'}" class="progress-bar"></div>
             <div class="progress-data">
               <span :class="{'fw-bold': maxPollIndex === index, 'fw-normal': maxPollIndex !== index}">{{ poll.choice_label }}</span>
-              <span :class="{'fw-bold': maxPollIndex === index, 'fw-normal': maxPollIndex !== index}">{{ Math.ceil(((settings.onlineMode ? state.polls[index] : poll.count)/pollCount)*100) + '%' }}</span>
+              <span :class="{'fw-bold': maxPollIndex === index, 'fw-normal': maxPollIndex !== index}">{{ Math.ceil((poll.count/pollCount)*100) + '%' }}</span>
             </div>
           </div>
         </div>
@@ -28,14 +28,12 @@
 </template>
 
 <script setup lang="ts">
-import {Media, PollItem} from "@/type/Content";
-import {computed, PropType, reactive, watch} from "vue";
+import {Media as MediaInterface, PollItem} from "@/type/Content";
+import {computed, PropType} from "vue";
 import {useStore} from "@/store";
 import {useI18n} from "vue-i18n";
-import {createRealMediaPath, Notice} from "@/share/Tools";
+import {createRealMediaPath} from "@/share/Tools";
 import {secondsToText} from "@/share/Time";
-import {request} from "@/share/Fetch";
-import {ApiPolls} from "@/type/Api";
 
 const props = defineProps({
   tweetId: {
@@ -44,21 +42,14 @@ const props = defineProps({
   },
   polls: {
     type: Array as PropType<PollItem[]>,
-    default: () => ([])
+    default: []
   },
   media: {
-    type: Array as PropType<Media[]>,
-    default: () => []
+    type: Array as PropType<MediaInterface[]>,
+    default: []
   }
 })
 
-const state = reactive<{
-  polls: number[]
-  updateFlag: boolean
-}>({
-  polls: [],
-  updateFlag: false
-})
 
 const {t} = useI18n()
 const store = useStore()
@@ -67,31 +58,8 @@ const settings = computed(() => store.state.settings)
 const realMediaPath = computed(() => store.state.realMediaPath)
 const samePath = computed(() => store.state.samePath)
 
-const pollCount = computed(() => {
-  if (settings.value.onlineMode && state.polls.length > 0) {
-    return state.polls.reduce((a, b) => a + b, 0)
-  } else {
-    return props.polls.map(x => x.count).reduce((a, b) => a + b, 0)
-  }
-})
+const pollCount = computed(() => props.polls.map(x => x.count).reduce((a, b) => a + b, 0))
 const etaSeconds = computed(() => (props.polls[0].end_datetime * 1000 - Number(now.value)) / 1000)
-
-if (settings.value.onlineMode) {
-  watch(etaSeconds, () => {
-    if (etaSeconds.value < 0 && state.polls.length === 0 && !state.updateFlag) {
-      state.updateFlag = true
-      request<ApiPolls>(settings.value.basePath + '/api/v3/data/poll/?tweet_id=' + props.tweetId).then(response => {
-        if (response.code === 200) {
-          state.polls = response.data
-        } else {
-          Notice(response.message, "error")
-        }
-      }).catch(e => {
-        Notice(String(e), "error")
-      })
-    }
-  })
-}
 
 const etaContent = (seconds: 1 | 60 | 3600 | 86400 = 1) => {
   return t("polls.vote", {count: pollCount.value}, pollCount.value > 1 ? 2 : 1) + ' · ' + t("polls.eta", [Math.ceil(etaSeconds.value / seconds) + ' ' + t('public.time.' + secondsToText[seconds], Math.ceil(etaSeconds.value / seconds) === 1 ? 1 : 2)])
@@ -114,7 +82,7 @@ const eta = computed(() => {
 
 const maxPollIndex = computed(() => {
   let tmpMaxIndex = -1
-  let tmpPolls = state.polls
+  let tmpPolls = props.polls.map(poll => poll.count)
   if (!settings.value.onlineMode) {
     tmpPolls = props.polls.map(poll => poll.count)
   }
